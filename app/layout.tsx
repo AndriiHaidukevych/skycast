a4a36b26cdb458b862548ecf14c5c66b88ca19ad
Andrii <andrii.haidukevych@tech-stack.io>
2026-05-21 15:39:50 +0200

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,11 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=block"
         />
       </head>
       <body
